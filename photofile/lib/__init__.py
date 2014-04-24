9e81f924fc7044cef75ceacad78176764765f767
Thomas Weholt <thomas@weholt.org>
2014-04-24 15:29:03 +0200

    Removed stale photofile folder.

--- photofile/lib/__init__.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-months = {}
-if not months:
-    for month in range(1, 12 + 1):
-        date = datetime(1900, month, 1)
-        months[month] = date.strftime('%B')
