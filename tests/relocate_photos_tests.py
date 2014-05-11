d68d4b8b0c6821a0fdff98f6b73ad66914d86a26
Thomas A. Weholt <thomas@weholt.org>
2014-05-12 00:14:50 +0200

    More unittests, but these are failing right now.

--- tests/relocate_photos_tests.py
+++ tests/relocate_photos_tests.py
@@ -3,6 +3,7 @@ import unittest
 import logging
 from optparse import OptionParser, OptionGroup
 import shutil
+import sys
 from mediaphile.lib import months
 from mediaphile.cli import default_timestamp_format, default_duplicate_filename_format, default_new_filename_format
 from mediaphile.lib.photos import get_date_from_file, relocate_photos
@@ -32,7 +33,14 @@ class RelocatePhotosTests(unittest.TestCase):
     def file_exists(self, filename):
         if os.path.exists(filename):
             return True
+
         print("File not found: %s" % filename)
+        counter = 0
+        while (counter < 10):
+            p,fname = os.path.split(filename)
+            parts = [o for o in p.split(os.sep)[:-1] if o != '.' and o]
+            print(os.path.join(os.curdir, os.sep.join(parts)))
+            counter += 1
 
     def test_relocate_photos(self):
         """
@@ -81,6 +89,146 @@ class RelocatePhotosTests(unittest.TestCase):
 
         self.assertTrue(self.file_exists(os.path.join(self.testarea, 'Summer', str(dt.year), months[dt.month], str(dt.day),
                                      generate_filename_from_date(self.testfile, dt,
+                                                                 timestamp_format=default_timestamp_format,
+                                                                 new_filename_format=default_new_filename_format))))
+
+    def test_relocate_photos_with_path_prefix_and_tag(self):
+        """
+
+        """
+        dt = get_date_from_file(self.testfile)
+
+        relocate_photos(
+            source_dir=self.testarea,
+            append_timestamp=True,
+            remove_source=False,
+            tag='SpecialTag',
+            dry_run=False,
+            photo_extensions_to_include=['JPG', 'jpg'],
+            timestamp_format=default_timestamp_format,
+            duplicate_filename_format=default_duplicate_filename_format,
+            new_filename_format=default_new_filename_format,
+            path_prefix='Summer',
+            skip_existing=False,
+            auto_tag=False)
+
+        self.assertTrue(self.file_exists(os.path.join(self.testarea, 'Summer', str(dt.year), months[dt.month], 'SpecialTag',
+                                     generate_filename_from_date(self.testfile, dt,
+                                                                 timestamp_format=default_timestamp_format,
+                                                                 new_filename_format=default_new_filename_format))))
+
+    def test_relocate_photos_with_tag(self):
+        """
+
+        """
+        dt = get_date_from_file(self.testfile)
+
+        relocate_photos(
+            source_dir=self.testarea,
+            append_timestamp=True,
+            remove_source=False,
+            tag='SpecialTag',
+            dry_run=False,
+            photo_extensions_to_include=['JPG', 'jpg'],
+            timestamp_format=default_timestamp_format,
+            duplicate_filename_format=default_duplicate_filename_format,
+            new_filename_format=default_new_filename_format,
+            path_prefix=None,
+            skip_existing=False,
+            auto_tag=False)
+
+        self.assertTrue(self.file_exists(os.path.join(self.testarea, str(dt.year), months[dt.month], 'SpecialTag',
+                                     generate_filename_from_date(self.testfile, dt,
+                                                                 timestamp_format=default_timestamp_format,
+                                                                 new_filename_format=default_new_filename_format))))
+
+    def test_relocate_photos_with_auto_tag(self):
+        """
+
+        """
+        new_tag = 'Tagged'
+        new_folder = os.path.join(self.testarea, new_tag)
+        os.makedirs(new_folder)
+        new_target_file = os.path.join(new_folder, os.path.split(self.testfile)[1])
+        shutil.move(self.testfile, new_target_file)
+        dt = get_date_from_file(new_target_file)
+
+        relocate_photos(
+            source_dir=new_folder,
+            append_timestamp=True,
+            remove_source=False,
+            tag=None,
+            dry_run=False,
+            photo_extensions_to_include=['JPG', 'jpg'],
+            timestamp_format=default_timestamp_format,
+            duplicate_filename_format=default_duplicate_filename_format,
+            new_filename_format=default_new_filename_format,
+            path_prefix=None,
+            skip_existing=False,
+            auto_tag=True)
+
+        self.assertTrue(self.file_exists(os.path.join(self.testarea, str(dt.year), months[dt.month], new_tag,
+                                     generate_filename_from_date(new_target_file, dt,
+                                                                 timestamp_format=default_timestamp_format,
+                                                                 new_filename_format=default_new_filename_format))))
+
+    def test_relocate_photos_with_no_auto_tag(self):
+        """
+
+        """
+        new_tag = 'Tagged'
+        new_folder = os.path.join(self.testarea, new_tag)
+        os.makedirs(new_folder)
+        new_target_file = os.path.join(new_folder, os.path.split(self.testfile)[1])
+        shutil.move(self.testfile, new_target_file)
+        dt = get_date_from_file(new_target_file)
+
+        relocate_photos(
+            source_dir=new_folder,
+            append_timestamp=True,
+            remove_source=False,
+            tag=None,
+            dry_run=False,
+            photo_extensions_to_include=['JPG', 'jpg'],
+            timestamp_format=default_timestamp_format,
+            duplicate_filename_format=default_duplicate_filename_format,
+            new_filename_format=default_new_filename_format,
+            path_prefix=None,
+            skip_existing=False,
+            auto_tag=False)
+
+        self.assertTrue(self.file_exists(os.path.join(self.testarea, str(dt.year), months[dt.month], str(dt.day),
+                                     generate_filename_from_date(new_target_file, dt,
+                                                                 timestamp_format=default_timestamp_format,
+                                                                 new_filename_format=default_new_filename_format))))
+
+    def test_relocate_photos_with_auto_tag_and_custom_tag(self):
+        """
+
+        """
+        new_tag = 'Tagged'
+        new_folder = os.path.join(self.testarea, new_tag)
+        os.makedirs(new_folder)
+        new_target_file = os.path.join(new_folder, os.path.split(self.testfile)[1])
+        shutil.move(self.testfile, new_target_file)
+        dt = get_date_from_file(new_target_file)
+
+        relocate_photos(
+            source_dir=new_folder,
+            append_timestamp=True,
+            remove_source=False,
+            tag="CustomTag",
+            dry_run=False,
+            photo_extensions_to_include=['JPG', 'jpg'],
+            timestamp_format=default_timestamp_format,
+            duplicate_filename_format=default_duplicate_filename_format,
+            new_filename_format=default_new_filename_format,
+            path_prefix=None,
+            skip_existing=False,
+            auto_tag=True)
+
+        self.assertTrue(self.file_exists(os.path.join(self.testarea, str(dt.year), months[dt.month], new_tag, "CustomTag",
+                                     generate_filename_from_date(new_target_file, dt,
                                                                  timestamp_format=default_timestamp_format,
                                                                  new_filename_format=default_new_filename_format))))
 
